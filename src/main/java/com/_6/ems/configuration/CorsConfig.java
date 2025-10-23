379a9c8fba200faf488d1ef67fc5d477417d6c44
PGeorge1709 <phucthanh0917@gmail.com>
2025-10-23 22:18:39 +0700

    add more allow origins

--- src/main/java/com/_6/ems/configuration/CorsConfig.java
+++ src/main/java/com/_6/ems/configuration/CorsConfig.java
@@ -17,7 +17,13 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://software-project-management-human-r.vercel.app/"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "http://localhost:5175",
+                "http://localhost:5176",
+                "https://software-project-management-human-r.vercel.app/",
+                "https://software-project-management-human-r-nu.vercel.app/"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 
